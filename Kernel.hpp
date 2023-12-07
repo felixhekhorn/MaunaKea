09e4c19f43ce1b7f0516df9a0c6ffd77fe403718
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2023-12-07 16:44:14 +0200

    Reshuffle code

---
+++ Kernel.hpp
@@ -0,0 +1,164 @@
+#ifndef KERNEL_HPP_
+#define KERNEL_HPP_
+
+#include <memory>
+#include <vector>
+
+#include "./FO.hpp"
+#include "./PineAPPL.hpp"
+#include "./config.h"
+
+/**
+ * @brief Integration kernel
+ */
+class Kernel : public HepSource::Integrand {
+  /** @brief heavy quark mass */
+  dbl m2;
+  /** @brief number of light flavors */
+  uint nl;
+  /** @brief active orders by bit */
+  uint order_mask;
+  /** @brief active flavors by bit */
+  uint flavor_mask;
+  /** @brief hadronic energy parameter \f$\rho_h = 4m^2/S_h\f$ */
+  dbl rho_h;
+  /** @brief hadronic energy parameter \f$\beta_h = \sqrt{1-\rho_h}\f$ */
+  dbl beta_h;
+  /** @brief grid */
+  std::unique_ptr<PineAPPL::Grid> grid;
+  /** @brief reference PDF */
+  std::unique_ptr<LHAPDF::PDF> pdf;
+  /** @brief strong coupling constant */
+  dbl as;
+
+ public:
+  /**
+   * @brief Constructor
+   * @param m2 heavy quark mass
+   * @param nl number of light flavors
+   */
+  Kernel(cdbl m2, cuint nl, cuint order_mask, cuint flavor_mask)
+      : m2(m2), nl(nl), order_mask(order_mask), flavor_mask(flavor_mask) {}
+
+  /**
+   * @brief Set hadronic Mandelstam \f$S_h\f$
+   * @param Sh hadronic Mandelstam S
+   */
+  void setHadronicS(cdbl Sh) {
+    this->rho_h = 4. * this->m2 / Sh;
+    this->beta_h = sqrt(1. - this->rho_h);
+  }
+
+  /**
+   * @brief Set reference PDF
+   * @param setname PDF set name
+   * @param member PDF member
+   */
+  void setPDF(const str setname, cuint member) {
+    this->pdf.reset(LHAPDF::mkPDF(setname, member));
+    this->as = this->pdf->alphasQ2(this->m2);
+  }
+
+  /**
+   * @brief was a PDF set?
+   * @return PDF set?
+   */
+  bool hasPDF() const { return static_cast<bool>(this->pdf); }
+
+  /**
+   * @brief Kernel function in Dvegas
+   * @param x adapted continuous integration variables
+   * @param k discrete integration variables
+   * @param weight integration weight
+   * @param aux unadapted continuous integration variables
+   * @param f output
+   */
+  void operator()(const double x[], const int k[], const double& weight, cdbl aux[], double f[]) {
+    (void)k;
+    (void)aux;
+    cdbl beta_min = 0.;
+    cdbl beta_max = this->beta_h;
+    cdbl beta = beta_min + (beta_max - beta_min) * x[0];
+    cdbl rho = 1 - beta * beta;
+    cdbl tau = rho_h / rho;
+    cdbl x1_min = tau;
+    cdbl x1_max = 1.;
+    cdbl x1 = x1_min + (x1_max - x1_min) * x[1];
+    cdbl x2 = tau / x1;
+    cdbl raw_jac = 2. * beta / rho / x1;
+    cdbl mu2 = this->m2;
+    cdbl c = f0gg(rho);
+    cdbl common_weight = 0.38937966e9 * c * raw_jac / this->m2 * (beta_max - beta_min) * (x1_max - x1_min);
+    this->grid->fill(x1, x2, mu2, 0, 0.5, 0, common_weight * weight * x1 * x2);
+    cdbl pdf_weight = common_weight * this->pdf->xfxQ2(21, x1, mu2) * this->pdf->xfxQ2(21, x2, mu2) * pow(this->as, 2);
+    f[0] = pdf_weight;
+  }
+
+  /** @brief Initialize grid */
+  void initGrid() {
+    // create a new luminosity function
+    PineAPPL::Lumi lumi;
+    lumi.add({PineAPPL::LumiEntry{21, 21, 1.0}});
+
+    // only LO
+    std::vector<PineAPPL::Order> orders = {PineAPPL::Order{2, 0, 0, 0}};
+
+    // fully-inclusive cross-section
+    std::vector<double> bins = {0.0, 1.0};
+
+    // create the PineAPPL grid
+    PineAPPL::KeyVal kv;
+    kv.set_double("x_min", rho_h);
+    this->grid.reset(new PineAPPL::Grid(lumi, orders, bins, kv));
+  }
+
+  /** @brief Optimize grid */
+  void optimizeGrid() const { this->grid->optimize(); }
+
+  /** @brief Write grid to disk */
+  void writeGrid(const str fp) const { this->grid->write(fp); }
+
+  /** @see HepSource::Integrand::Dvegas_init */
+  void Dvegas_init() const { this->grid->scale(0.); }
+
+  /** @see HepSource::Integrand::Dvegas_final */
+  void Dvegas_final(cuint iterations) const { this->grid->scale(1. / iterations); }
+};
+
+// if ((p->partonchannel)=="qqbar")
+// {
+// f = (p->Flux).fluxqqbar(x) * (p->FO).FOqqbar((p->rho)/x) ;
+// }
+// else if ((p->partonchannel)=="gg")
+// {
+// f = (p->Flux).fluxgg(x) * (p->FO).FOgg((p->rho)/x) ;
+// }
+// else if ((p->partonchannel)=="qg")
+// {
+// f = (p->Flux).fluxgq(x) * (p->FO).FOgq((p->rho)/x) ;
+// }
+// else if ((p->partonchannel)=="qq")
+// {
+// f = (p->Flux).fluxqq(x) * (p->FO).FOqq((p->rho)/x) ;
+// }
+// else if ((p->partonchannel)=="qqprime")
+// {
+// f = (p->Flux).fluxqqprime(x) * (p->FO).FOqqprime((p->rho)/x) ;
+// }
+// else if ((p->partonchannel)=="qqbarprime")
+// {
+// f = (p->Flux).fluxqqbarprime(x) * (p->FO).FOqqbarprime((p->rho)/x) ;
+// }
+// else//all channels combined
+// {
+// f = (p->Flux).fluxqqbar(x)    * (p->FO).FOqqbar((p->rho)/x)      +
+// (p->Flux).fluxgg(x)         * (p->FO).FOgg((p->rho)/x)         +
+// (p->Flux).fluxgq(x)         * (p->FO).FOgq((p->rho)/x)         +
+// (p->Flux).fluxqq(x)         * (p->FO).FOqq((p->rho)/x)         +
+// (p->Flux).fluxqqprime(x)    * (p->FO).FOqqprime((p->rho)/x)    +
+// (p->Flux).fluxqqbarprime(x) * (p->FO).FOqqbarprime((p->rho)/x) ;
+// }
+// return f/x;
+// }
+
+#endif  // KERNEL_HPP_
---
+++ MaunaKea.hpp
@@ -0,0 +1,62 @@
+#ifndef MAUNAKEA_HPP_
+#define MAUNAKEA_HPP_
+
+#include "./Integration.hpp"
+#include "./Kernel.hpp"
+
+class MaunaKea {
+  /** @brief integration kernel */
+  Kernel k;
+
+ public:
+  /** @brief Integration configurtation */
+  IntegrationConfig intCfg;
+  /** @brief Integration output */
+  IntegrationOutput intOut;
+
+  /**
+   * @brief Constructor
+   * @param m2 heavy quark mass
+   * @param nl number of light flavors
+   */
+  MaunaKea(cdbl m2, cuint nl, cuint order_mask, cuint flavor_mask) : k(m2, nl, order_mask, flavor_mask) {
+    this->intCfg.MC_warmupCalls = 1000;
+    this->intCfg.calls = 20000;
+  }
+
+  /**
+   * @brief Set hadronic Mandelstam \f$S_h\f$
+   * @param Sh hadronic Mandelstam S
+   */
+  void setHadronicS(cdbl Sh) { this->k.setHadronicS(Sh); }
+
+  /**
+   * @brief Set reference PDF
+   * @param setname PDF set name
+   * @param member PDF member
+   */
+  void setPDF(const str setname, cuint member) { this->k.setPDF(setname, member); }
+
+  /** @brief Run calculation */
+  void run() {
+    // Checks
+    if (!this->k.hasPDF()) throw std::domain_error("No PDF was set so far!");
+    // Init
+    this->k.initGrid();
+    // do it!
+    IntegrationOutput out;
+    integrate2D(&this->k, this->intCfg, &out);
+    this->intOut = out;
+    // post-process
+    this->k.optimizeGrid();
+    // grid.set_key_value("events", "10000000");
+  }
+
+  /**
+   * @brief Write grid to disk
+   * @param fp file path
+   */
+  void write(const str fp) const { this->k.writeGrid(fp); }
+};
+
+#endif  // MAUNAKEA_HPP_
