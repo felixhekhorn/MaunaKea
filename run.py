f326919d41654f32d33540a6b41ae8182f92f4f3
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2024-04-23 12:55:22 +0300

    Update example script

--- run.py
+++ run.py
@@ -1,21 +1,25 @@
+"""Dummy test script."""
+
 import MaunaKea
 
 
 def run() -> None:
-    nl: int = 3
-    m2: float = 1.51**2
-    S_h: float = 8e3**2
+    """Dummy test function."""
+    nl: int = 3  # number of light flavors
+    m2: float = 1.51**2  # [GeV^2] mass of heavy quark
+    S_h: float = 8e3**2  # [GeV^2] collider energy
     # init object
     mk = MaunaKea.MaunaKea(m2, nl, MaunaKea.ORDER_LO, MaunaKea.LUMI_ALL)
-    # mk.intCfg.calls = 5000;
+    # configure
+    # mk.intCfg.calls = 5000
     mk.intCfg.verbosity = 3
     mk.setHadronicS(S_h)
     mk.setPDF("NNPDF40_nnlo_as_01180", 0)
-    # mk.setCentralScaleRatio(2.);
+    mk.setCentralScaleRatio(2.0)
     # fill the grid
     mk.run()
-    intOut = mk.getIntegrationOutput()
-    print("sigma_tot = {:e} +- {:e} [pb]\n".format(intOut.result, intOut.error))
+    output = mk.getIntegrationOutput()
+    print(f"sigma_tot = {output.result:e} +- {output.error:e} [pb]")
     # save
     mk.write("MaunaKea.pineappl.lz4")
 
