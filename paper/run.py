a65ac9a56d9cd8df31b49e5493e9af8556c68d41
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2024-04-24 16:18:59 +0300

    Improve runner

--- paper/run.py
+++ paper/run.py
@@ -1,8 +1,12 @@
 """Compute grids."""
 
 import argparse
+import os
 import pathlib
+import time
+from multiprocessing import Pool
 
+import lhapdf
 import numpy as np
 import pineappl
 
@@ -10,6 +14,8 @@ import MaunaKea
 
 LABELS = {3: "ccbar", 4: "bbbar"}
 MASSES = {3: 1.51**2, 4: 4.92**2}
+SH_MIN: float = 20.0**2
+SH_MAX: float = 400e3**2
 PDFS = {3: "NNPDF40_nnlo_pch_as_01180_nf_3", 4: "NNPDF40_nnlo_as_01180_nf_4"}
 
 
@@ -23,29 +29,46 @@ def grid_path(nf: int) -> str:
     return f"{LABELS[nf]}.pineappl.lz4"
 
 
-def compute(nl: int, ndata: int) -> None:
+def compute(nl: int, ndata: int, processes: int = -1) -> None:
     """Compute grids."""
-    m2: float = MASSES[nl]
-    Sh_min: float = 20.0**2
-    Sh_max: float = 1e4**2
-    for j in range(ndata):
-        logS_h: float = np.log(Sh_min) + (np.log(Sh_max) - np.log(Sh_min)) * j / (
-            ndata - 1
+    if processes <= 0:
+        processes = max(os.cpu_count() + processes, 1)
+    start = time.perf_counter()
+    with Pool(processes) as p:
+        p.starmap(
+            compute_subgrid,
+            zip(
+                range(ndata),
+                [ndata] * ndata,
+                [nl] * ndata,
+                np.geomspace(SH_MIN, SH_MAX, ndata),
+            ),
         )
-        S_h: float = np.exp(logS_h)
-        print(f"j = {j:d}, S = {S_h:e}")
-        # init object
-        mk = MaunaKea.MaunaKea(m2, nl, MaunaKea.ORDER_ALL, MaunaKea.LUMI_ALL)
-        mk.intCfg.calls = 50000
-        mk.setHadronicS(S_h)
-        mk.setPDF(PDFS[nl], 0)
-        mk.setCentralScaleRatio(2.0)
-        # fill the grid
-        mk.run()
-        int_out = mk.getIntegrationOutput()
-        print(f"sigma_tot = {int_out.result:e} +- {int_out.error:e} [pb]")
-        # save
-        mk.write(sub_grid_path(nl, j))
+    delta = time.perf_counter() - start
+    print("---")
+    print(f"computed {ndata} grids in {delta/60:.2f} min")
+
+
+def compute_subgrid(j: int, ndata: int, nl: int, sh: float) -> None:
+    """Compute a subgrid."""
+    m2: float = MASSES[nl]
+    print(f"j = {j:d}/{ndata:d}, S = {sh:e} GeV^2")
+    lhapdf.setVerbosity(0)
+    start = time.perf_counter()
+    # init object
+    mk = MaunaKea.MaunaKea(m2, nl, MaunaKea.ORDER_ALL, MaunaKea.LUMI_ALL)
+    mk.intCfg.calls = 5000
+    mk.setHadronicS(sh)
+    mk.setPDF(PDFS[nl], 0)
+    mk.setCentralScaleRatio(2.0)
+    # fill the grid
+    mk.run()
+    int_out = mk.getIntegrationOutput()
+    delta = time.perf_counter() - start
+    print(f"sigma_tot = {int_out.result:e} +- {int_out.error:e} [pb]")
+    print(f"took {delta:.2f} s and chi2iter={int_out.chi2iter}")
+    # save
+    mk.write(sub_grid_path(nl, j))
 
 
 def merge(nl: int, ndata: int) -> None:
@@ -56,9 +79,9 @@ def merge(nl: int, ndata: int) -> None:
         sub_grid_path_ = pathlib.Path(sub_grid_path(nl, j))
         grid = pineappl.grid.Grid.read(sub_grid_path_)
         # rebin by S_h
-        sqrt_s_h = np.sqrt(float(grid.key_values()["MaunaKea/hadronicS"]))
-        print(f"Merging j={j} with √S_h={sqrt_s_h} GeV")
-        br = pineappl.bin.BinRemapper([1.0], [(sqrt_s_h, sqrt_s_h)])
+        sqrt_sh = np.sqrt(float(grid.key_values()["MaunaKea/hadronicS"]))
+        print(f"Merging j={j} with √S_h={sqrt_sh} GeV")
+        br = pineappl.bin.BinRemapper([1.0], [(sqrt_sh, sqrt_sh)])
         grid.set_remapper(br)
         # start or continue?
         if base is None:
@@ -81,12 +104,13 @@ def merge(nl: int, ndata: int) -> None:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("nl", help="Number of light flavors.")
-    parser.add_argument("ndata", help="Number of points.")
-    parser.add_argument("-c", "--compute", help="Compute grids.", action="store_true")
-    parser.add_argument("-m", "--merge", help="Merge grids.", action="store_true")
+    parser.add_argument("nl", help="Number of light flavors")
+    parser.add_argument("ndata", help="Number of points")
+    parser.add_argument("-c", "--compute", help="Compute grids", action="store_true")
+    parser.add_argument("-m", "--merge", help="Merge grids", action="store_true")
+    parser.add_argument("--processes", default=-1, help="Number of parallel threads")
     args = parser.parse_args()
     if args.compute:
-        compute(int(args.nl), int(args.ndata))
+        compute(int(args.nl), int(args.ndata), int(args.processes))
     if args.merge:
         merge(int(args.nl), int(args.ndata))
