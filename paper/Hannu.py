ff322ec6e707a4fd51166b03fc71acf49627f0a4
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2025-02-13 13:31:42 +0200

    Add SV to Hannu's script

--- paper/Hannu.py
+++ paper/Hannu.py
@@ -15,6 +15,8 @@ grid = pineappl.grid.Grid.read("ccbar-1.30.pineappl.lz4")
 sqrts = grid.bin_left(0)
 # restrict to NLO
 order_mask = pineappl.grid.Order.create_mask(grid.orders(), 2, 0, True)
+# choose scale
+xi = 1.0
 # do it! we just need to undo one optimization, since the grid was generated for p-p:
 # We can reuse the same grid also for p-X collision if we average over the two input state.
 # E.g. the LO (q-qbar) channel is just that and we need the q in coming from both sides.
@@ -26,6 +28,7 @@ pPb = (
         nuclear_pdf.xfxQ2,
         p_pdf.alphasQ2,
         order_mask=order_mask,
+        xi=[(xi, xi)],
     )
     + grid.convolve_with_two(
         2212,
@@ -34,9 +37,10 @@ pPb = (
         p_pdf.xfxQ2,
         p_pdf.alphasQ2,
         order_mask=order_mask,
+        xi=[(xi, xi)],
     )
 ) / 2.0
 # output
 data = np.array([sqrts, pPb])
-np.savetxt("Hannu.txt", data.T)
+np.savetxt(f"Hannu-xi_{xi}.txt", data.T)
 print(data.T)
