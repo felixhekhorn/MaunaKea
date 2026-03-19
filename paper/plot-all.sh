95b2e430759982572145897e6020cfeefdb9bdff
Felix Hekhorn <felix.a.hekhorn@jyu.fi>
2026-03-19 14:04:52 +0200

    Improve plot bash script

--- paper/plot-all.sh
+++ paper/plot-all.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
-# Iterate all groups. Pass `pto`, `alphas`, or `mass` as argument.
+# Iterate all PDF groups and quarks.
+# 1. argument: `pto`, `lumi`, `alphas`, or `mass`
+# 2. argument: `+` (normal mode) or `-` (data mode)
 
 # charm
-python plot.py 1.51 3 --$1 --short-range
-python plot.py 1.4 3 --$1 --short-range
-python plot.py 1.3 3 --$1 --pdf_set CT18NNLO_rescaled_NF3 --short-range
+python plot.py 1.51 ${2}3 --$1 --short-range
+python plot.py 1.40 ${2}3 --$1 --short-range
+python plot.py 1.30 ${2}3 --$1 --pdf_set CT18NNLO_rescaled_NF3 --short-range
 
 # bottom
-python plot.py 4.92 4 --$1 --short-range
-python plot.py 4.75 4 --$1 --short-range
-python plot.py 4.75 4 --$1 --pdf_set CT18NNLO_rescaled_NF4 --short-range
+python plot.py 4.92 ${2}4 --$1 --short-range
+python plot.py 4.75 ${2}4 --$1 --short-range
+python plot.py 4.75 ${2}4 --$1 --pdf_set CT18NNLO_rescaled_NF4 --short-range
